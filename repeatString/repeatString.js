5d65cd64ea8ac150245aa26b0600c70fbd820592
Rob Elmore <robelmore23@gmail.com>
2021-08-04 12:20:44 -0400

    solved problems 1 and 2

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+  if ( count < 0 ){
+    return 'ERROR'
+}
+  
+    let name = ''
+   for ( let i = 0; i < count; i++){
+       name += str  
+   }
+   return name 
 };
 
 module.exports = repeatString;
