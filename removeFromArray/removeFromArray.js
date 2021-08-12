3701dd829efad5e7739d9bb7879333bf7f0ccbe9
Rob Elmore <robelmore23@gmail.com>
2021-08-12 13:32:29 -0400

    removed array

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, toBeRemoved) {
+    // arr [1,2,3,4]
+    // toBeRemoved 2
+    //return [1,3,4]
 
+
+    const newArray = []
+
+    arr.forEach((item) => {
+        if (item !== toBeRemoved) {
+            newArray.push(item)
+        }
+    })
+
+    return newArray 
+    
 };
 
 module.exports = removeFromArray;
