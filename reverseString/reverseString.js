c2000faa6339efc478e9b14b2103767d507ae8e2
Rob Elmore <robelmore23@gmail.com>
2021-08-12 12:56:31 -0400

    my changes to reverse string

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    // const splitString = str.split("")
+    // console.log(splitString)
+    
+    // const reverse = splitString.reverse()
+    // console.log(reverse)
 
+    // const join = reverse.join("")
+    // console.log(join)
+
+    // return join 
+
+    return str.split("").reverse().join("")
 };
 
 module.exports = reverseString;
+    
